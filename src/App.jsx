e3fba0b0fb92b270566f69b6fc6f3e47f2b50d31
Sardar balnour <balnour.sardar@gmail.com>
2026-07-04 12:13:36 +0330

    GET

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => setTodos(res.data));
+  }, []);
+
   return (
     <>
       <h1>Sardar the dev</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
